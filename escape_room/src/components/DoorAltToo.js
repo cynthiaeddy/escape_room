414795543ed5dce46b765869979ad8b6991efc4e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-03-28 22:56:36 -0400

    big circle moves

--- escape_room/src/components/DoorAltToo.js
+++ escape_room/src/components/DoorAltToo.js
@@ -7,9 +7,26 @@ import door_open from '../assets/door_open.jpg'
 const DoorAltToo = () => {
 
   const [isSelected, setSelected] = useState(true);
+  const [isCircleMoved, setCircleMoved] = useState(true)
+
+  const startStyle = {
+    top: '160px',
+    left: '40px',
+    transition: 'left 1s'
+
+
+  }
+  const endStyle = {
+    top: '160px',
+    left: '700px',
+    transition: 'left 1s',
+  }
 
   const toggleDoor = () => {
     setSelected(!isSelected);
+  }
+  const moveCircle = () => {
+    setCircleMoved(!isCircleMoved)
   }
 
   const doors = isSelected ? door_closed : door_open;
@@ -17,6 +34,8 @@ const DoorAltToo = () => {
     <div className='door_container'>
       <button onClick={toggleDoor} className='key_button'>
       </button>
+      <button onClick={moveCircle} className='circle move_right'
+        style={isCircleMoved ? startStyle : endStyle}></button>
         <img alt={'doors'} src={doors} />
     </div>
   )
